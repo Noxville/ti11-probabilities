6a94feb913258d37cc7c5f44437e7fe90b88343b
Noxville <bensteenhuisen@gmail.com>
2022-07-14 02:59:22 +0200

    QC vs EG

--- differ.py
+++ differ.py
@@ -1,5 +1,10 @@
 import json
 
+
+def fmt(i):
+    return int(i * 10000) / 100
+
+
 if __name__ == "__main__":
     with open('./data/index.json') as f_index:
         idx = json.load(f_index)
@@ -12,10 +17,10 @@ if __name__ == "__main__":
             pred = json.load(fin)
             its = pred['iterations']
             for team, outcomes in pred['team_outcomes'].items():
-                val = qual_shifts.get(team, 0)
-                qual_shifts[team] = val + sign * (outcomes['points_qual'] / its)
+                val = qual_shifts.get(team, list())
+                val.append(outcomes['points_qual'] / its)
+                qual_shifts[team] = val
 
     print("Shifts in direct (points) qual. %")
-    for team, shift in sorted(qual_shifts.items(), key=lambda x: abs(x[1]), reverse=True):
-        print(f"{team}: {int(shift * 10000)/100}%")
-
+    for team, shift in sorted(qual_shifts.items(), key=lambda x: abs(x[1][1]-x[1][0]), reverse=True):
+        print(f"{team}: {fmt(shift[1] - shift[0])}% ({fmt(shift[0])}% -> {fmt(shift[1])})% ")
